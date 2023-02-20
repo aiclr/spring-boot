716c5aff83b6f34e51cc429298591e5658acee45
caddy <caddyren@qq.com>
2023-02-20 09:37:34 +0800

    fixbug: add ignore class

--- spring-boot-project/spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/LaunchedURLClassLoader.java
+++ spring-boot-project/spring-boot-tools/spring-boot-loader/src/main/java/org/springframework/boot/loader/LaunchedURLClassLoader.java
@@ -87,7 +87,7 @@ public class LaunchedURLClassLoader extends URLClassLoader {
 			"STCloudBoxPayOrder.class", "ETCPayOrder.class", "PlatformPaidPayOrder.class", "InnerPaidPayOrder.class",
 			"CouponPayOrder.class", "TimesCardPayOrder.class", "PrepaidOrder.class", "JustOutPayOrder.class",
 			"EPayPayOrder.class", "CouponPayOrderProxy.class", "ChangXingPayOrder4WeChat.class",
-			"StcbEtcUploadPayment.class", "MakeupInLogDto.class", "CostUsedDetail.class");
+			"StcbEtcUploadPayment.class", "MakeupInLogDto.class", "CostUsedDetail.class", "PermitEnum.class");
 
 	private static boolean isSkip(String name) {
 		String str = name + ".class";
